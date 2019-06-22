9df48ca2eeb6bed5474c4fd3685647282f6e3d30
Renato Böhler <deploy@travis-ci.org>
2019-06-22 22:33:01 +0000

    Deploy renato-bohler/mapa-dados-abertos to github.com/renato-bohler/mapa-dados-abertos.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mapa-dados-abertos/precache-manifest.fa6a0c838ee3f7a4f43c6d6dae89b8b2.js"
+  "/mapa-dados-abertos/precache-manifest.3e149f1621266cbc774f61c26f79d1cf.js"
 );
 
 self.addEventListener('message', (event) => {
